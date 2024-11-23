f026ea95da3b35ff19679625609f8469ba20eb47
JKorboe <jkorb001@gold.ac.uk>
2024-11-23 21:19:33 +0000

    Update hmenu.js

--- hmenu.js
+++ hmenu.js
@@ -1,5 +1,8 @@
 function toggleMenu() {
+    onsole.log("Hamburger menu clicked!"); 
     document.querySelector('.menu-content').classList.toggle('open');
 }
 
 document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
+
+
