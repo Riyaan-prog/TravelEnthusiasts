3ad2ac2530c186ef2ec9b77ca366e9e6f8416808
JKorboe <jkorb001@gold.ac.uk>
2024-11-23 21:10:40 +0000

    Update hmenu.js

--- hmenu.js
+++ hmenu.js
@@ -1,6 +1,5 @@
 function toggleMenu() {
-    var menuContent = document.querySelector('.menu-content').classList.toggle('open');
+    document.querySelector('.menu-content').classList.toggle('open');
 }
-
 
 document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
